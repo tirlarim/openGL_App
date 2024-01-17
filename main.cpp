db17e7f322fd339dffc9be910ca3b52bb69cc312
Stanislav Romanov <stasromanov801@gmail.com>
2024-01-17 14:41:52 +0100

    fix lang stats calculation

--- main.cpp
+++ main.cpp
@@ -18,13 +18,13 @@ int main() {
 //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //?
   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
   if (window == NULL) {
-    std::cout << "Failed to create GLFW window" << std::endl;
+    std::cerr << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
   glfwMakeContextCurrent(window);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
+    std::cerr << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
   glViewport(0, 0, 800, 600);
