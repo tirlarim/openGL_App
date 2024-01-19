fe1e6e55934b877a0251349af1c5895f66c5816d
Stanislav Romanov <stasromanov801@gmail.com>
2024-01-19 12:02:52 +0100

    draw square

--- src/main.c
+++ src/main.c
@@ -13,28 +13,32 @@ void frameBufferSizeCallback(GLFWwindow* window, i32 width, i32 height) {
 }
 
 void processInput(GLFWwindow* window) {
+  const static u32 allDrawModes[] = {GL_POINT, GL_LINE, GL_FILL};
+  static u8 currentModeIndex = 0;
+  static bool spaceKeyPressed = false;
+  bool spaceDown = glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS;
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+  if (spaceKeyPressed && !spaceDown) glPolygonMode(GL_FRONT_AND_BACK, allDrawModes[(++currentModeIndex) % 3]);
+  spaceKeyPressed = spaceDown;
 }
 
 void checkProgramCompilation(u32 shaderProgram) {
-  const i32 logSize = 512;
   int success;
-  char infoLog[logSize];
+  char infoLog[MAX_ERR_LOG_LEN];
   glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
   if (!success) {
-    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    glGetProgramInfoLog(shaderProgram, MAX_ERR_LOG_LEN, NULL, infoLog);
     fprintf(stderr, "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n%s\n", infoLog);
     abort();
   }
 }
 
 void checkShaderCompilation(u32 shader) {
-  const i32 logSize = 512;
   int success;
-  char infoLog[logSize];
+  char infoLog[MAX_ERR_LOG_LEN];
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   if (!success) {
-    glGetShaderInfoLog(shader, logSize, NULL, infoLog);
+    glGetShaderInfoLog(shader, MAX_ERR_LOG_LEN, NULL, infoLog);
     fprintf(stderr, "ERROR::SHADER::COMPILATION_FAILED\n%s\n", infoLog);
     abort();
   }
@@ -71,18 +75,27 @@ u32 compileShaderProgram() { // rewrite as cycle
 }
 
 u32 setupTriangle() {
-  f32 triangleVer[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f
+  f32 vertices[] = {
+      0.5f, 0.5f, 0.0f,  // top right
+      0.5f, -0.5f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f,// bottom left
+      -0.5f, 0.5f, 0.0f  // top left
   };
-  u32 vbo, vao; // vertex buffer objects (VBO)
+  u32 indices[] = {
+      0, 1, 3,   // first triangle
+      1, 2, 3    // second triangle
+  };
+  u32 vbo, vao, ebo; // vertex buffer objects (VBO) | vertex array object | element buffer object
   glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
-  // copy our vertices array in a buffer for OpenGL to use
+  // copy our vertices array in a VBO for OpenGL to use
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVer), triangleVer, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // copy our indices array in a EBO for OpenGL to use
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   // then set our vertex attributes pointers
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(f32), NULL);
   glEnableVertexAttribArray(0);
@@ -97,19 +110,19 @@ int main() {
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Test OpenGL App", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(SCR.WIDTH, SCR.HEIGHT, "Test OpenGL App", NULL, NULL);
   u32 shaderProgram;
   u32 vao;
   if (window == NULL) {
     fprintf(stderr, "Failed to create GLFW window\n");
     glfwTerminate();
-    return -1;
+    abort();
   }
   glfwMakeContextCurrent(window);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     fprintf(stderr, "Failed to initialize GLAD\n");
     glfwTerminate();
-    return -1;
+    abort();
   }
   glClearColor(COLOR_GREEN_MAIN);
   glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
@@ -122,7 +135,8 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(shaderProgram);
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0); // ?
     // check and call events and swap the buffers
     glfwPollEvents();
     glfwSwapBuffers(window);
